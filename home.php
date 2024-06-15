67b9dc6a52d75cced656fa9a0b9b2a4fc86439fa
Vaithyanathan Ramanathan <vaithy31@gmail.com>
2024-06-16 00:44:30 +0530

    Initial Commit

---
+++ home.php
@@ -0,0 +1,5 @@
+<a href="whoami.html">WHOAMI</a>
+<a href="album.html">ALBUM</a>
+<a href="resume.html">RESUME</a>
+<a href="connect.html">CONNECT</a>
+<a href="restricted.html">RESTRICTED</a>
---
+++ login.php
@@ -0,0 +1,8 @@
+<form>
+  <label for="user_name">User Name:</label><br>
+  <input type="text" id="user_name" name="user_name"><br><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password"><br><br>
+  <input type="submit" value="Submit">&emsp;<input type="reset" value="Reset">
+</form>
+<p> <?php echo "Hello"; ?> </p>
